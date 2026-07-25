3542b875c8b363f2b36e86a4e2dab504361767d9
iqnite <iqnitetechnologies@gmail.com>
2026-07-25 16:51:43 +0200

    Format

--- ai-server/core.py
+++ ai-server/core.py
@@ -1,52 +1,59 @@
-from dotenv import load_dotenv
-import requests
 import json
 import os
+
+import requests
+from dotenv import load_dotenv
 
 # Load environment
 load_dotenv()
 API_KEY = os.getenv("REPLICATE_API_TOKEN")
 
+
 # Functions
 def llm_request(user_prompt: str) -> str:
     headers = {
-        'Authorization': f'Bearer {API_KEY}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
     }
 
     json_data = {
-        'model': '~google/gemini-flash-latest',
-        'messages': [
+        "model": "~google/gemini-flash-latest",
+        "messages": [
             {
-                'role': 'user',
-                'content': user_prompt,
+                "role": "user",
+                "content": user_prompt,
             },
         ],
     }
 
-    response = requests.post('https://ai.hackclub.com/proxy/v1/chat/completions', headers=headers, json=json_data)
+    response = requests.post(
+        "https://ai.hackclub.com/proxy/v1/chat/completions",
+        headers=headers,
+        json=json_data,
+    )
     return json.loads(response.text)["choices"][0]["message"]["content"]
+
 
 def tts_request(prompt: str) -> str:
     headers = {
-        'Authorization': 'Bearer ' + API_KEY,
-        'Content-Type': 'application/json',
-        'Prefer': 'wait',
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+        "Prefer": "wait",
     }
 
     json_data = {
-        'input': {
-            'text': prompt,
-            'voice_id': 'English_ImposingManner',
-            'language_boost': 'English',
-            'english_normalization': True,
+        "input": {
+            "text": prompt,
+            "voice_id": "English_ImposingManner",
+            "language_boost": "English",
+            "english_normalization": True,
         }
     }
 
     response = requests.post(
-        'https://ai.hackclub.com/proxy/v1/replicate/models/minimax/speech-2.8-turbo/predictions',
+        "https://ai.hackclub.com/proxy/v1/replicate/models/minimax/speech-2.8-turbo/predictions",
         headers=headers,
-        json=json_data
+        json=json_data,
     )
 
     return json.loads(response.text)["output"]
