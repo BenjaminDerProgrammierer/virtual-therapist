964754623a7641bc26d392b31f16ebdd7037e520
Miguel <loranvallsmiguel@gmail.com>
2026-07-26 00:58:53 +0200

    Add dashboard placeholder

--- virtual-therapist-app/src/app/dashboard/page.tsx
+++ virtual-therapist-app/src/app/dashboard/page.tsx
@@ -6,7 +6,23 @@ export default function Dashboard() {
     <>
       <TopBar selectedPage="/dashboard"/>
       <main className={styles.main}>
-
+        <h1>Dashboard</h1>
+        <div className={styles.container}>
+          <section className={styles.conversationHistory}>
+            <h2>Conversation history</h2>
+            <p>
+              User: Testing 1 2 3<br/>
+              Dr. Snickers: Testing 4 5 6
+            </p>
+          </section>
+          <section className={styles.memory}>
+            <h2>Memory</h2>
+            <p>
+              - Testing 1 2 3<br/>
+              - Testing 4 5 6
+            </p>
+          </section>
+        </div>
       </main>
     </>
   );
