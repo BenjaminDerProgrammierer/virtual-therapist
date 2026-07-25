76b662232a3e6933cd2263669cae9f338a8e0bf4
Miguel <loranvallsmiguel@gmail.com>
2026-07-26 00:21:36 +0200

    CSSgit add .git add .git add .git add .!

--- virtual-therapist-app/src/app/page.tsx
+++ virtual-therapist-app/src/app/page.tsx
@@ -3,6 +3,7 @@ import layout from "./layout.module.css";
 import styles from "./page.module.css";
 import Image from 'next/image'
 import snickers from '../../public/snickers.jpg'
+import {Sparkles} from "lucide-react";
 
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <section className={styles.intro}>
           <p className={styles.eyebrow}>Clanker therapy</p>
-          <h1>Agentic <span className="underline">AI-powered</span> hamster therapy.</h1>
+          <h1>Agentic <Sparkles className={styles.aiaiai}/>AI-powered<Sparkles className={styles.aiaiai}/> hamster therapy.</h1>
           <p>
             Dr. Snickers is SOTA—in every therapy benchmark. Honestly? This not just another therapist—this will put you—ahead of everyone else—in 2026.
           </p>
